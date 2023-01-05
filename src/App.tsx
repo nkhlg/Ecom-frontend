e0cbaff0d62e8f1d18fb86ee58e3b5f41f44a069
nkhlg <114756264+nkhlg@users.noreply.github.com>
2023-01-05 18:05:02 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Navbar from './Components/Public/Navbar'
+import Home from './Components/Public/Home'
+
+
+function App() {
+  return (
+    <div className="App">
+     
+     <Navbar></Navbar>
+     <Home></Home>
+    </div>
+  );
+}
+
+export default App;
